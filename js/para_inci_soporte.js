36d275ae5096627d06d037d188883af686f4be5e
Patricio Soto <psotomerino@gmail.com>
2022-08-29 18:13:28 -0500

    8

--- js/para_inci_soporte.js
+++ js/para_inci_soporte.js
@@ -1,12 +1,17 @@
 jQuery(document).ready(function(){
     $('.contenedor').hide();
-    
+
     $(document).on('click','#btn_inci',function(){
         //lista_insidencias();
         $('.contenedor').show();        
         $('#home').hide();         
         
     }); 
+    $(document).on('click','#ini_',function(){
+        $('.contenedor').hide();      
+        $('#home').show(); 
+        
+    });
 
 
 /** FIN DE TODO */    
